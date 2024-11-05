80ccd0f377fd909d01f4baf702b0ed6f20ad91b6
samuelmachajohn <samuelmachajohn@gmail.com>
2024-11-05 14:53:28 +0100

    hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
+echo "# Hello-world-py" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/print ("hello world")Hello-world-py.git
+git push -u origin main
 print ("hello world")
